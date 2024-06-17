463ecedc0c9e34733c0844b5e24b5b90b12e8622
devrewoh <137814665+devrewoh@users.noreply.github.com>
2024-06-17 15:34:09 -0700

    added get_charcount function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     #print(text)
     print(f"Number of words counted in document: {get_wordcount(text)}")
+    print(f"Number of character instances counted in document: {get_charcount(text)}")
 
 
 def get_book_text(path):
@@ -12,6 +13,17 @@ def get_book_text(path):
 def get_wordcount(text):
     words = text.split()
     return len(words)
+
+def get_charcount(text):
+    lowered_string = text.lower()
+    char_count_dict = {}
+    for char in lowered_string:
+        if char in char_count_dict:
+            char_count_dict[char] += 1
+        else:
+            char_count_dict[char] = 1
+    return char_count_dict
+
 
 
 main()
